05c6f5d7a9310239934ca733cf4918ec11e8cebf
Kâlòÿän Pâvlòv <kaloian.pavlov@abv.bg>
2014-09-28 08:37:32 -0500

    Changes made by Kâlòÿän Pâvlòv (kaloian.pavlov@abv.bg).

--- WhereAmI/scripts/view-models/find.js
+++ WhereAmI/scripts/view-models/find.js
@@ -5,13 +5,10 @@ app.viewmodels = app.viewmodels || {};
 (function(scope) {
     function start() {
         function init(e) {
-            alert('go');
             var model = {
                 createGoogleMap: function() {
-                    //alert('here');
                     var latlng;
                     getPosition(function(street, position) {
-                        alert('sadasdsadasdas');
                         latlng = position;
                         var myOptions = {
                             zoom: 18,
@@ -23,7 +20,7 @@ app.viewmodels = app.viewmodels || {};
 
                         var map = new google.maps.Map(div,
                                                       myOptions);
-                        console.log(street); 
+                        console.log(street);
                     });
                 }
             }
@@ -31,12 +28,11 @@ app.viewmodels = app.viewmodels || {};
         
             kendo.bind(e.view.element, vm);
         }
-    
         scope.initKendo = {
             initMaps: init
         };
     }
     
     google.maps.event.addDomListener(window, 'load', start);
-    //document.addEventListener('deviceready', start, false);
+    document.addEventListener('deviceready', start, false);
 }(app.viewmodels));
