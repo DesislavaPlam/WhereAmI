c39ae9bc2acc6b2e8fbe26182422ae9be46aad30
Desislava Plam <desislava.plam@gmail.com>
2014-09-28 05:38:05 -0500

    Changes made by Desislava Plam (desislava.plam@gmail.com).

---
+++ WhereAmI/scripts/viewmodels/take-picture.js
@@ -0,0 +1,49 @@
+var app = app || {};
+
+app.viewmodels = app.viewmodels || {};
+
+(function(scope) {
+    var onSuccess = function onSuccess(imageURI) {
+        var image = document.getElementById('myImage');
+        image.src = imageURI;
+    }
+        
+    var onFail = function onFail(message) {
+        alert('Failed because: ' + message);
+    }
+    
+    function init(e) {
+        //debugger;
+        var model = {
+            addImage: function() {
+                //debugger;
+                var configuration = {
+                    quality: 50,
+                    destinationType: Camera.DestinationType.NATIVE_URL,
+                    sourceType : Camera.PictureSourceType.CAMERA
+                };
+            
+                navigator.camera.getPicture(
+                    onSuccess, onFail, configuration);
+            },  
+            getImage: function() {
+                //debugger;
+                var configuration = {
+                    quality: 50,
+                    destinationType: Camera.DestinationType.NATIVE_URL,
+                    sourceType : Camera.PictureSourceType.PHOTOLIBRARY 
+                };
+                navigator.camera.getPicture(
+                    onSuccess, onFail, configuration);
+            }    
+        }
+        var vm = kendo.observable(model);
+        
+        kendo.bind(e.view.element, vm);
+    }
+    
+    scope.takePicture = {
+        title: "Where am I?",
+        init: init
+    };
+}(app.viewmodels));
